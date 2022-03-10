80a09290738b3d67d4673cbf73a0b71131bf6b29
Szebasztian Lukity <lukicseba@gmail.com>
2022-03-09 22:59:00 -0500

    added the order controller, order model and order view to the website

---
+++ models/order.model.js
@@ -0,0 +1,34 @@
+const db = require('../database/database');
+class Order {
+  constructor(cart, userData, status = 'pending', date, orderId) {
+    this.productData = cart;
+    this.userData = userData;
+    this.status = status;
+    this.date = new Date(date);
+    if (this.date) {
+      this.formattedDate = this.date.toLocaleDateString('en-CA', {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      });
+    }
+    this.id = orderId;
+  }
+
+  save() {
+    if (this.id) {
+    } else {
+      const orderDocument = {
+        userData: this.userData,
+        productData: this.productData,
+        date: new Date(),
+        status: this.status,
+      };
+
+      return db.getDb().collection('orders').insertOne(orderDocument);
+    }
+  }
+}
+
+module.exports = Order;
