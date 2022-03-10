63712bd934207ae7d157207577df771b955bc8ed
Szebasztian Lukity <lukicseba@gmail.com>
2022-03-10 15:33:57 -0500

    added the stripe payment api logic

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,7 @@
+const stripe = require('stripe')(
+  'sk_test_51KbsWEDMf7PSu6H0fzIGHQULTiRnfOY9EW6Xb4IRCwzMygRRChGEy8b4dzxAlwDXPBYK90gzQ6Nsp590WlaEydjf00FtRttd1T'
+);
+
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
@@ -13,6 +17,8 @@ async function getOrders(req, res) {
 }
 
 async function addOrder(req, res, next) {
+  const cart = res.locals.cart;
+
   let userDocument;
   try {
     userDocument = await User.findById(res.locals.uid);
@@ -31,10 +37,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map(function (item) {
+      return {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: 'cad',
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure,
 };
